b5789af9807bbbf68c942e4b00e77fafce38b9d2
Leo <leo.hh@hhress.com>
2026-06-02 16:07:59 +0800

    feat: 更新 core 版本，將啟動提取為共用 func

--- go.mod
+++ go.mod
@@ -3,22 +3,22 @@ module telegram
 go 1.26.1
 
 require (
-	buf.build/gen/go/leo84927-proto/scheduler/protocolbuffers/go v1.36.11-20260420071308-c2dd85fa7562.1
+	buf.build/gen/go/leo84927-proto/scheduler/protocolbuffers/go v1.36.11-20260514041544-eb667470b4d7.1
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
-	github.com/leo84927/core v1.4.1
-	github.com/rotisserie/eris v0.5.4
+	github.com/leo84927/core v1.5.0
 	github.com/shopspring/decimal v1.4.0
-	golang.org/x/sync v0.20.0
 	google.golang.org/protobuf v1.36.11
 )
 
 require (
+	filippo.io/edwards25519 v1.2.0 // indirect
 	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/cenkalti/backoff/v5 v5.0.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-sql-driver/mysql v1.10.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.28.0 // indirect
@@ -31,10 +31,12 @@ require (
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+	github.com/rotisserie/eris v0.5.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/contrib/bridges/otelslog v0.17.0 // indirect
 	go.opentelemetry.io/otel v1.42.0 // indirect
@@ -47,6 +49,7 @@ require (
 	go.opentelemetry.io/proto/otlp v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20260218203240-3dfff04db8fa // indirect
 	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20260209200024-4cfbd4190f57 // indirect
--- main.go
+++ main.go
@@ -3,120 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"log/slog"
 	"os"
-	"os/signal"
-	"runtime/debug"
-	"syscall"
 	"telegram/handle"
-	"time"
 
-	"github.com/leo84927/core/config"
-	"github.com/leo84927/core/logger"
-	"github.com/leo84927/core/rabbitmq"
-	"github.com/rotisserie/eris"
-	"golang.org/x/sync/errgroup"
+	"github.com/leo84927/core/initialize"
 )
 
 func init() {
 	// 啟動時先清理，防止上次異常結束殘留
 	if err := os.Remove("/tmp/ready"); err != nil && !os.IsNotExist(err) {
-        panic(err)
-    }
-}
-
-// 包裝 errgroup，就可以不用每個 goroutine 都宣告 defer recover
-func graceful(g *errgroup.Group, fn func() error) {
-	g.Go(func() (err error) {
-		defer func() {
-			if r := recover(); r != nil {
-				err = fmt.Errorf("recovered: %v\n%s", r, debug.Stack())
-				log.Println(err)
-				slog.Error(
-					"panic happened",
-					"error", eris.ToJSON(err, true),
-				)
-			}
-		}()
-
-		return fn()
-	})
+		panic(err)
+	}
 }
 
 func main() {
-	defer func() {
-		if r := recover(); r != nil {
-			err := fmt.Errorf("recovered: %v\n%s", r, debug.Stack())
-			log.Println(err)
-			slog.Error(
-				"panic happened",
-				"error", eris.ToJSON(err, true),
-			)
-		}
-	}()
-
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	lm := logger.NewLogManager(&logger.Config{
-		ServiceName: config.ServiceName,
-		Host:        config.AlloyHost,
-		Port:        config.AlloyPort,
-	})
-	err := lm.SetLogger(ctx)
+	app, err := initialize.New()
 	if err != nil {
-		log.Fatalf("SetLogger failed, err: %v\n", err)
-	}
-	defer lm.CloseLogger(ctx)
-
-	cm := rabbitmq.NewConnectionManager(config.GetRabbitMQConfig().Config)
-	defer cm.Close()
-
-	connReady := make(chan struct{})
-	consumer := cm.NewConsumer(config.GetRabbitMQConfig().ServiceQueue.Name, "", 5, 20*time.Second)
-
-	group, groupCtx := errgroup.WithContext(ctx)
-
-	graceful(group, func() error {
-		// 建立連線＆拓樸
-		if err := cm.InitTopology(groupCtx, config.GetRabbitMQConfig().Topology); err != nil {
-			return err
-		}
-
-		// 建立 ready 檔案用來做 health check
-		if err := os.WriteFile("/tmp/ready", []byte("ok"), 0644); err != nil {
-			return err
-		}
-
-		// 連線就緒，關閉 connReady channel，讓 consumer 知道可以啟動了
-		close(connReady)
-
-		slog.Info("rabbitmq connection and topology ready")
-		return cm.WatchConnAndRetry(groupCtx)
-	})
-
-	// 拓樸建立後才能訂閱 queue 並常駐 consumer
-	graceful(group, func() error {
-		<-connReady
-
-		slog.Info("consumer start")
-		return consumer.WaitForConsume(groupCtx, handle.MessageHandler)
-	})
-
-	// 定期發送心跳給 consul
-	graceful(group, func() error {
-		return config.Client.SendHeartbeat(groupCtx, config.ServiceName, 30*time.Second)
-	})
-
-	// 等待所有 goroutine 結束
-	if err := group.Wait(); err != nil {
-		slog.Error(
-			"shutdown with err",
-			"error", eris.ToJSON(err, true),
-		)
+		fmt.Fprintln(os.Stderr, err)
 		return
 	}
+	defer app.Close()
 
-	slog.Info("normal shutdown")
+	app.Workers = []func(ctx context.Context) error{
+		func(ctx context.Context) error {
+			return app.Consumer.WaitForConsume(ctx, handle.MessageHandler)
+		},
+	}
+	app.Run()
 }
